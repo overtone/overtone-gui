ca281055ef30396ab5aae008e5bfff63edd43b0d
Sam Aaron <samaaron@gmail.com>
2011-02-17 17:48:44 +0000

    get mult-scopes working

--- src/overtone/gui/scope.clj
+++ src/overtone/gui/scope.clj
@@ -1,15 +1,15 @@
 (ns
-  ^{:doc "An oscilloscope style waveform viewer"
-     :author "Jeff Rose & Sam Aaron"}
+    ^{:doc "An oscilloscope style waveform viewer"
+      :author "Jeff Rose & Sam Aaron"}
   overtone.gui.scope
   (:import
-    (java.awt Graphics Dimension Color BasicStroke)
-    (java.awt.geom Rectangle2D$Float Path2D$Float)
-    (javax.swing JFrame JPanel))
+   (java.awt Graphics Dimension Color BasicStroke)
+   (java.awt.geom Rectangle2D$Float Path2D$Float)
+   (javax.swing JFrame JPanel))
   (:use
-     [overtone event util time-utils]
-     [overtone.sc core synth ugen buffer node]
-    clojure.stacktrace)
+   [overtone event util time-utils]
+   [overtone.sc core synth ugen buffer node]
+   clojure.stacktrace)
   (:require [overtone.log :as log]
             [clojure.set :as set]))
 
@@ -23,7 +23,7 @@
 (def X-PADDING 5)
 (def Y-PADDING 10)
 
-; Some utility synths for signal routing and scoping
+;; Some utility synths for signal routing and scoping
 (defsynth bus->buf [bus 20 buf 0]
   (record-buf (in bus) buf))
 
@@ -32,7 +32,6 @@
 
 (defn- update-scope [s]
   (let [{:keys [buf size width height panel y-array x-array panel]} s
-        _ (println "buf:" buf)
         frames (buffer-data buf)
         step (int (/ (:size buf) width))
         y-scale (/ (- height (* 2 Y-PADDING)) -2)
@@ -48,8 +47,6 @@
   (doall (map update-scope (vals @scopes*))))
 
 (defn- paint-scope [g id]
-  (println "trying to paint" id )
-  (println "scope: "  (get @scopes* id))
   (if-let [scope (get @scopes* id)]
     (let [{:keys [background width height color x-array y-array]} scope]
       (.setColor ^Graphics g ^Color background)
@@ -62,10 +59,10 @@
 (defn- wait-for-buffer [b]
   (loop [i 0]
     (cond
-      (= 20 i) nil
-      (not (buffer-ready? b)) (do
-                                (java.lang.Thread/sleep 50)
-                                (recur (inc i))))))
+     (= 20 i) nil
+     (not (buffer-ready? b)) (do
+                               (java.lang.Thread/sleep 50)
+                               (recur (inc i))))))
 
 (def SCOPE-BUF-SIZE 4096)
 
@@ -161,7 +158,7 @@
            num  (if (= -1 buf) bus buf)
            s (mk-scope num kind keep-on-top WIDTH HEIGHT)]
        (swap! scopes* assoc (:id s) s)
-       (start-scopes-runner))))
+       (scopes-start))))
 
 
 
@@ -170,38 +167,38 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (comment
-(require 'examples.basic)
+  (require 'examples.basic)
 
-(defonce test-frame (JFrame. "scope"))
-(defonce _test-scope (do
-             (.setPreferredSize test-panel (Dimension. 600 400))
-             (.add (.getContentPane test-frame) test-panel)
-             (.setScene test-panel (scope))
-             (.pack test-frame)
-             (.show test-frame)))
+  (defonce test-frame (JFrame. "scope"))
+  (defonce _test-scope (do
+                         (.setPreferredSize test-panel (Dimension. 600 400))
+                         (.add (.getContentPane test-frame) test-panel)
+                         (.setScene test-panel (scope))
+                         (.pack test-frame)
+                         (.show test-frame)))
 
-(defn- go-go-scope []
-  (let [b (buffer 2048)]
-    (Thread/sleep 100)
-    (scope-buf b)
-    (scope-on)
-    (examples.basic/sizzle :bus 20)
-    (bus->buf 20 (:id b))
-    (bus->bus 20 0)))
+  (defn- go-go-scope []
+    (let [b (buffer 2048)]
+      (Thread/sleep 100)
+      (scope-buf b)
+      (scope-on)
+      (examples.basic/sizzle :bus 20)
+      (bus->buf 20 (:id b))
+      (bus->bus 20 0)))
 
-(defn- spectrogram [in-bus]
-  (let [fft-buf (buffer 2048)
-        buf (buffer 2048)]
-    (Thread/sleep 100)
-    (freq-scope-zero in-bus fft-buf buf)))
+  (defn- spectrogram [in-bus]
+    (let [fft-buf (buffer 2048)
+          buf (buffer 2048)]
+      (Thread/sleep 100)
+      (freq-scope-zero in-bus fft-buf buf)))
 
-(defn test-scope []
-  (if (not (connected?))
-    (do
-      (boot)
-      (on :examples-ready go-go-scope))
-    (go-go-scope))
-  (.show test-frame))
+  (defn test-scope []
+    (if (not (connected?))
+      (do
+        (boot)
+        (on :examples-ready go-go-scope))
+      (go-go-scope))
+    (.show test-frame))
   )
 
 
@@ -209,51 +206,51 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Spectragraph stuff to be worked on
 (comment
-  ; Note: The fft ugen writes into a buffer:
-; dc, nyquist, real, imaginary, real, imaginary....
-(comment defn- update-scope []
-  (let [{:keys [buf width height panel]} @scope*
-        frames  (buffer-data buf)
-        n-reals (/ (- (:size buf) 2) 2)
-        step    (int (/ n-reals width))
-        y-scale (/ (- height (* 2 Y-PADDING)) 2)
-        y-shift (+ (/ height 2) Y-PADDING)]
-    (dotimes [x width]
-      (aset ^ints y-array x
-            (int (+ y-shift
-                    (* y-scale
-                       (aget ^floats frames
-                             (+ 2 (* 2 (unchecked-multiply x step))))))))))
-  (.repaint (:panel @scope*)))
+                                        ; Note: The fft ugen writes into a buffer:
+                                        ; dc, nyquist, real, imaginary, real, imaginary....
+  (comment defn- update-scope []
+           (let [{:keys [buf width height panel]} @scope*
+                 frames  (buffer-data buf)
+                 n-reals (/ (- (:size buf) 2) 2)
+                 step    (int (/ n-reals width))
+                 y-scale (/ (- height (* 2 Y-PADDING)) 2)
+                 y-shift (+ (/ height 2) Y-PADDING)]
+             (dotimes [x width]
+               (aset ^ints y-array x
+                     (int (+ y-shift
+                             (* y-scale
+                                (aget ^floats frames
+                                      (+ 2 (* 2 (unchecked-multiply x step))))))))))
+           (.repaint (:panel @scope*)))
 
-(defsynth freq-scope-zero [in-bus 0 fft-buf 0 scope-buf 1
-                           rate 4 phase 1 db-factor 0.02]
-  (let [n-samples (* 0.5 (- (buf-samples:kr fft-buf) 2))
-        signal (in in-bus)
-        freqs  (fft fft-buf signal 0.75 :hann)
-;        chain  (pv-mag-smear fft-buf 1)
-        phasor (+ (+ n-samples 2)
-                  (* n-samples
-                     (lf-saw (/ rate (buf-dur:kr fft-buf)) phase)))
-        phasor (round phasor 2)]
-    (scope-out (* db-factor (ampdb (* 0.00285 (buf-rd 1 fft-buf phasor 1 1))))
-               scope-buf)))
-
-
-(defsynth freqs [in-bus 10 fft-buf 0]
-  (let [n-samples (* 0.5 (- (buf-samples:kr fft-buf) 2))
-        signal    (in in-bus 1)]
-    (fft fft-buf signal 0.75 :hann)))
+  (defsynth freq-scope-zero [in-bus 0 fft-buf 0 scope-buf 1
+                             rate 4 phase 1 db-factor 0.02]
+    (let [n-samples (* 0.5 (- (buf-samples:kr fft-buf) 2))
+          signal (in in-bus)
+          freqs  (fft fft-buf signal 0.75 :hann)
+                                        ;        chain  (pv-mag-smear fft-buf 1)
+          phasor (+ (+ n-samples 2)
+                    (* n-samples
+                       (lf-saw (/ rate (buf-dur:kr fft-buf)) phase)))
+          phasor (round phasor 2)]
+      (scope-out (* db-factor (ampdb (* 0.00285 (buf-rd 1 fft-buf phasor 1 1))))
+                 scope-buf)))
 
 
+  (defsynth freqs [in-bus 10 fft-buf 0]
+    (let [n-samples (* 0.5 (- (buf-samples:kr fft-buf) 2))
+          signal    (in in-bus 1)]
+      (fft fft-buf signal 0.75 :hann)))
 
-(defsynth scoper-outer [buf 0]
-  (scope-out (sin-osc 200) buf))
-(scope-out)
 
-(defn freq-scope-buf [buf]
+
+  (defsynth scoper-outer [buf 0]
+    (scope-out (sin-osc 200) buf))
+  (scope-out)
+
+  (defn freq-scope-buf [buf]
+    )
+
+
   )
-
-
-)
 
